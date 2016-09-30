00b2e47e7d3891f6b454703de0a94679022ac7df
Andreas Mueller <amueller@nyu.edu>
2016-09-30 16:47:15 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mglearn",
+    version="0.1",
+    packages=find_packages(),
+    requires=['numpy', 'matplotlib', 'scikit-learn'],
+
+    author="Andreas Mueller",
+    author_email="t3kcit@gmail.com",
+    description=("Helper functions for the book "
+                 "'Introduction to machine learning with Python'"),
+    license="BSD",
+    keywords="machine learning ml sklearn scikit-learn",
+    url="https://github.com/amueller/introduction_to_ml_with_python",
+)
