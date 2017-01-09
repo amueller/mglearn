09927da0106cd8d6b1c2030308d3c55c85e5dda5
Andreas Mueller <t3kcit@gmail.com>
2017-01-09 18:49:26 -0500

    fix manifest path, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mglearn",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'scikit-learn'],
 
