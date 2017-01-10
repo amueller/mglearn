6f26356a82720377381df0136dd0b51f17b67182
Andreas Mueller <t3kcit@gmail.com>
2017-01-09 19:06:38 -0500

    bump version again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mglearn",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'scikit-learn', 'pandas',
                       'pillow'],
