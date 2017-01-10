922bdfc663d5e4fae7f0dc909fd0f0fe1fefceef
Andreas Mueller <t3kcit@gmail.com>
2017-01-09 19:01:41 -0500

    bump version again, also include version in __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mglearn",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'scikit-learn'],
     include_package_data=True,
