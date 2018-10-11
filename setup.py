fd33165600b11676213a733129b27cf23ebc6768
Andreas Mueller <andreas.mueller@columbia.edu>
2018-10-11 14:28:49 -0400

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mglearn",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'scikit-learn', 'pandas',
                       'pillow', 'cycler', 'imageio'],
