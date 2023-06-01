be98bbb81439a10dc0ea9d8d18a692c0e3d93abb
Andreas Mueller <amueller@microsoft.com>
2023-05-31 19:36:56 -0700

    try to fix description for pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
     author="Andreas Mueller",
     author_email="t3kcit@gmail.com",
     description=("Helper functions for the book "
-                 "'Introduction to machine learning with Python'"),
+                 "Introduction to machine learning with Python"),
+    long_description=("Helper functions for the book "
+                      "`Introduction to machine learning with Python`"),
+    long_description_content_type="text/markdown",
     license="BSD",
     keywords="machine learning ml sklearn scikit learn",
     url="https://github.com/amueller/introduction_to_ml_with_python",
